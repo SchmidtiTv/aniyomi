78327e8b83818759322d0046fb86db54e0dbd9ec
Schmidti <91827260+schmidtitv@users.noreply.github.com>
2026-06-03 14:58:10 +0200

    refactor: Removed unnecessary Change

--- presentation-core/src/main/java/tachiyomi/presentation/core/components/ListGroupHeader.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/components/ListGroupHeader.kt
@@ -1,19 +1,11 @@
 package tachiyomi.presentation.core.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import tachiyomi.presentation.core.components.material.padding
 
 @Composable
@@ -21,27 +13,15 @@ fun ListGroupHeader(
     text: String,
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    Text(
+        text = text,
         modifier = modifier
-            .fillMaxWidth()
             .padding(
                 horizontal = MaterialTheme.padding.medium,
                 vertical = MaterialTheme.padding.small,
             ),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        Text(
-            text = text,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            fontWeight = FontWeight.SemiBold,
-            style = MaterialTheme.typography.bodyMedium,
-        )
-        Box(
-            modifier = Modifier
-                .height(2.dp)
-                .weight(1f)
-                .background(MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.12f)),
-        )
-    }
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+        fontWeight = FontWeight.SemiBold,
+        style = MaterialTheme.typography.bodyMedium,
+    )
 }
